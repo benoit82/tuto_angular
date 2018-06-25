416d6c104b462e597680889ba4235e8b21469684
Benoit <benoit@Benoit-LM18.3>
2018-06-25 22:03:18 +0200

    ajout du premier module

--- mon-projet-angular/src/app/app.module.ts
+++ mon-projet-angular/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent
   ],
   imports: [
     BrowserModule,
